ef706bfb65273bfef2007f61fa36b524d3d4c168
forresti <fiandola@gmail.com>
2014-03-28 12:19:15 -0700

    better job of printing out layer sizes... removing unused code.

---
+++ resize_net.sh
@@ -0,0 +1,6 @@
+
+
+#../build/examples/resize_net.bin ./imagenet_val.prototxt
+#../build/examples/resize_net.bin ./imagenet_deploy.prototxt
+../build/examples/resize_net.bin ./imagenet_rcnn_batch_1_input_2000x2000_output_conv5.prototxt
+
