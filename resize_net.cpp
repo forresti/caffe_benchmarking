d07483fee7017d10d4c165f08877ec6a977c06ac
forresti <fiandola@gmail.com>
2014-03-28 12:45:08 -0700

    found the points where conv5 goes from 12x12 to 13x13, to 14x14, and to 15x15

--- resize_net.cpp
+++ resize_net.cpp
@@ -71,11 +71,22 @@ int main(int argc, char** argv) {
     //print_layer_sizes(caffe_net_resized); //updated sizes
     #endif
 
+    // inputSz=214 -> conv5=12x12x256
+
+    // inputSz=215 -> conv5=13x13x256
+    // inputSz=230 -> conv5=13x13x256
+
+    // inputSz=231 -> conv5=14x14x256
+    // inputSz=246 -> conv5=14x14x256
+
+    // inputSz=247 -> conv5=15x15x256
+
     //look for size where we go from conv5=13x13x256 to conv5=14x14x256.
-    for(int inputSz = 227; inputSz<300; inputSz++){
+    for(int inputSz = 210; inputSz<250; inputSz++){
         net_param.set_input_dim(2, inputSz); //height
         net_param.set_input_dim(3, inputSz); //width
         Net<float> caffe_net_resized(net_param); //initialize net (and print most of the blob sizes too)
+        print_layer_sizes(caffe_net_resized); //includes bottom and top printouts
     }
 
     return 0;
