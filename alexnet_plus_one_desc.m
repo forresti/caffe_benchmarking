0edcd1ef022b46439fd195c0c65bb7b78c9c6bd6
forresti <fiandola@gmail.com>
2014-03-28 14:47:51 -0700

    reorganize alexnet_plus_one

--- alexnet_plus_one_desc.m
+++ alexnet_plus_one_desc.m
@@ -1,47 +1,48 @@
 
-function white_pixel_test()
-    img = single(zeros(227, 227, 3));
-    img(100, 100, :) = 255.0; %one white pixel
-    img(200, 200, :) = 255.0; %one white pixel
+function alexnet_plus_one_desc()
+    %img = single(zeros(227, 227, 3));
+    %img(100, 100, :) = 255.0; %one white pixel
+    %img(200, 200, :) = 255.0; %one white pixel
+    img = imread('/media/big_disk/installers_old/caffe/voc-release5/cachedir/VOC2007/JPEGImages/000023.jpg');
+    %figure(1)
+    %imshow(img);
+    %title('original image')
 
-    figure(1)
-    imshow(img);
-    title('original image')
-
-    AlexNet_setup();
-    alexnet_desc{1} = AlexNet_descriptor(img); 
-
-    figure(4)
-    colormap(gray)
-    %imagesc(sum(abs(alexnet_desc{1}), 3)')
-    im = max(abs(alexnet_desc{1}), [], 3)';
-    imagesc(log(im / max(im(:))))
-    colorbar
-    title('conv5 max')
-
-%227x227, 1 window per batch.
-function AlexNet_setup()
-    %model_def_file = '../examples/imagenet_deploy.prototxt';
-    %model_def_file = '../../examples/imagenet_deploy_batchsize1_output_conv5.prototxt';
-    model_def_file = '/media/big_disk/installers_old/caffe/examples/imagenet_deploy_batchsize1_output_conv5.prototxt';
-    %model_def_file = '../../examples/imagenet_deploy_batchsize1_output_pool2.prototxt';
-    %model_def_file = '../../examples/imagenet_deploy_batchsize1_output_conv1.prototxt'; %227x227 -> 55x55
-    %model_def_file = '../examples/imagenet_deploy_batchsize1_input_500x500_output_conv1.prototxt'; %500x500 -> 123x123 
-    %model_def_file = '../../python/caffe/imagenet/imagenet_rcnn_batch_1_input_2000x2000_output_conv1.prototxt';
     model_file = '../../examples/alexnet_train_iter_470000';
+
+    %243x243 test
+    model_def_file = '/media/big_disk/installers_old/caffe/examples/imagenet_deploy_batchsize1_input_243x243_output_conv5.prototxt'; 
     caffe('init', model_def_file, model_file);
     caffe('set_mode_gpu');
+    img_243 = img(1:243, 1:243, :);
+    %img_243 = imresize(img_227, [243 243], 'bilinear');
+    desc_14 = caffe('forward', {img_243});
+    desc_14 = reshape(desc_14{1}, [14 14 256]); %243x243 -> 14x14 
+    show_desc(desc_14, 4, '243x243 -> 14x14, conv5');
+
+    %227x227 test
+    model_def_file = '/media/big_disk/installers_old/caffe/examples/imagenet_deploy_batchsize1_output_conv5.prototxt';
+    caffe('init', model_def_file, model_file);
+    caffe('set_mode_gpu');
+    img_227 = img(1:227, 1:227, :);
+    desc_13 = caffe('forward', {img_227});
+    desc_13 = reshape(desc_13{1}, [13 13 256]); %227x227 -> 13x13    
+    show_desc(desc_13, 2, '227x227 -> 13x13, conv5');
+
+function show_desc(desc, figID, titleName)
+    figure(figID)
+    colormap(gray)
+    %im = sum(abs(desc), 3)';
+    im = max(abs(desc), [], 3)';
+    imagesc(im);
+    %imagesc(log(im / max(im(:))))
+    colorbar
+    title(titleName)
 
 function desc = AlexNet_descriptor(myWindow)
 
     [h w d] = size(myWindow);
-    %assert( (h==227) && (w==227) );
-
-    % resize to fixed input size
     im = single(myWindow);
-    im = imresize(im, [227 227], 'bilinear');
-    %im = imresize(im, [500 500], 'nearest');
-    %im = imresize(im, [2000 2000], 'nearest');
     % permute from RGB to BGR (IMAGE_MEAN is already BGR)
     im = im(:,:,[3 2 1]); % - IMAGE_MEAN;
 
@@ -50,10 +51,6 @@ function desc = AlexNet_descriptor(myWindow)
 %    im(:,:,3) = im(:,:,3) - 122.67; % R
     im = permute(im, [2,1,3]); %[h w d] -> [d h w]
 
-    scores = caffe('forward', {im});
-    desc = reshape(scores{1}, [13 13 256]); %conv5
-    %desc = reshape(scores{1}, [55 55 96]); %conv1
-    %desc = reshape(scores{1}, [123 123 96]); %conv1, with 500x500
-    %desc = reshape(scores{1}, [498 498 96]); %conv1, with 2000x2000
-    
-
+    desc = caffe('forward', {im});
+    %returns flattened descriptor (your job to reshape it)
+ 
