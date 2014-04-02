72ba8c189d3651bc8746a3eaf3efd983bbc7eb3a
forresti <fiandola@gmail.com>
2014-04-01 22:18:00 -0700

    remove unused code

--- alexnet_plus_one_desc.py
+++ alexnet_plus_one_desc.py
@@ -13,19 +13,6 @@ def create_caffenet(model_def_file, pretrained_model):
     caffenet.set_mode_cpu()
     return caffenet
 
-'''
-def prepare_image(filename):
-    img = io.imread(filename)
-    if img.ndim == 2:
-        img = np.tile(img[:, :, np.newaxis], (1, 1, 3))
-    elif img.shape[2] == 4:
-        img = img[:, :, :3]
-
-    img.shape
-    img = (img*255)[:, :, ::-1] #RGB -> BGR
-    #img -= IMAGENET_MEAN #don't do data centering for now.
-    return img
-'''
 def prepare_image(img):
     if img.ndim == 2:
         img = np.tile(img[:, :, np.newaxis], (1, 1, 3))
@@ -55,12 +42,6 @@ caffenet = create_caffenet(MODEL_FILE, PRETRAINED)
 img = io.imread(IMAGE_FILE)
 input_blob = img[0:227, 0:227, :]
 input_blob = prepare_image(input_blob)
-#input_blob = input_blob[0:227, 0:227, :]
-#input_blob = input_blob.transpose((2,0,1))
-#input_blob = input_blob[:, 0:227, 0:227] #crop
-#input_blob = np.ascontiguousarray(input_blob)
-#input_blob = np.expand_dims(input_blob, axis=0) #(3,227,227) -> (1,3,227,227)
-#input_blob = input_blob.astype(np.float32)
 output_blob_dims = get_last_blob_dims(caffenet)
 output_blobs = [np.empty((output_blob_dims), dtype=np.float32)] 
 caffenet.Forward([input_blob], output_blobs) 
