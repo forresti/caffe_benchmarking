017f322538584dd2d09d7c930c70ae76395fae4c
forresti <fiandola@gmail.com>
2014-04-01 22:38:32 -0700

    diffing by layer. things look same up to pad1... then pad2 diverges a lot.

--- alexnet_plus_one_desc.py
+++ alexnet_plus_one_desc.py
@@ -31,27 +31,47 @@ def get_last_blob_dims(caffenet):
     layerName = caffenet.blobs.keys()[-1]
     return np.shape(caffenet.blobs[layerName].data)
 
+def diff_layer(reference, experimental, layerName):
+    print 'layer: %s' %layerName
+    dist_from_0 = dist = np.linalg.norm(experimental)
+    print '    L2 distance of alexnetPlusOne from 0: %f' %dist_from_0
+    dist = np.linalg.norm(reference - experimental) # L2 norm
+    print '    L2 distance of alexnetPlusOne from reference code: %f' %dist
+    return dist
 
 #MODEL_FILE = '/media/big_disk/installers_old/caffe_lowMemConv/examples/imagenet/imagenet_deploy.prototxt'
 MODEL_FILE = '/media/big_disk/installers_old/caffe/examples/imagenet_deploy_batchsize1_output_conv5.prototxt'
 PRETRAINED = '/media/big_disk/installers_old/caffe_lowMemConv/examples/alexnet_train_iter_470000'
 IMAGE_FILE = '/media/big_disk/installers_old/caffe/voc-release5/cachedir/VOC2007/JPEGImages/000023.jpg'
 
-caffenet = create_caffenet(MODEL_FILE, PRETRAINED)
-
 img = io.imread(IMAGE_FILE)
+
+# 227x227 (default alexnet)
+caffenet = create_caffenet(MODEL_FILE, PRETRAINED)
 input_blob = img[0:227, 0:227, :]
 input_blob = prepare_image(input_blob)
 output_blob_dims = get_last_blob_dims(caffenet)
 output_blobs = [np.empty((output_blob_dims), dtype=np.float32)] 
 caffenet.Forward([input_blob], output_blobs) 
-
-caffenet_227x227_data = caffenet.blobs
+caffenet_227x227_activations = caffenet.blobs
 #layerNames = caffenet.blobs.keys()
-#conv2_data = caffenet.blobs['conv2']
-#print conv2_data.data
-#SUCCESS. was able to pull out an individual layer's data.
+#conv2_activations = caffenet.blobs['conv2']
+#print conv2_activations.data
 
+# 243x243 (alexnet + 16 ... produces 14x14 instead of 13x13 conv5)
+MODEL_FILE = '/media/big_disk/installers_old/caffe/examples/imagenet_deploy_batchsize1_input_243x243_output_conv5.prototxt'
+caffenet = create_caffenet(MODEL_FILE, PRETRAINED)
+input_blob = img[0:243, 0:243, :]
+input_blob = prepare_image(input_blob)
+output_blob_dims = get_last_blob_dims(caffenet)
+output_blobs = [np.empty((output_blob_dims), dtype=np.float32)] 
+caffenet.Forward([input_blob], output_blobs) 
+caffenet_243x243_activations = caffenet.blobs
 
-#embed()
+for k in caffenet_227x227_activations.keys():
+    reference = caffenet_227x227_activations[k].data
+    refshape = reference.shape
+    experimental = caffenet_243x243_activations[k].data
+    experimental = experimental[:, :, 0:refshape[2], 0:refshape[3]] #top-left corner of experimental (larger) slice
+    dist = diff_layer(reference, experimental, k)
 
