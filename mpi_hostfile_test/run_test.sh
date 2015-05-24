e414ee3718b1620db784097e7f4c30bc9f2c20ca
forresti <forresti@eecs.berkeley.edu>
2015-05-23 19:59:56 -0700

    testing out the group of 5 FireBox nodes that now have GPUs

--- mpi_hostfile_test/run_test.sh
+++ mpi_hostfile_test/run_test.sh
@@ -2,8 +2,9 @@ policy=--byslot #a19: {0, 1}; a20: {2, 3} ... this is default
 #policy=--bynode #a19: {0, 2}; a20: {1, 3} ... round robin
 
 #hostfile=hostfiles/a19_a20_2slot.txt
-hostfile=hostfiles/a18_a19_a20_allslot.txt
+#hostfile=hostfiles/a18_a19_a20_allslot.txt
 #hostfile=hostfiles/a18_8slot.txt
+hostfile=hostfiles/f12_to_f16_1slot.txt
 
-mpirun --hostfile $hostfile -np 10 $policy ./main
+mpirun --hostfile $hostfile -np 5 $policy ./main
 
