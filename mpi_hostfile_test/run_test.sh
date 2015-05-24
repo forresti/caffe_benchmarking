c0e99868bca14f2e0b50e65a41d27b2c409374a4
forresti <forresti@eecs.berkeley.edu>
2015-05-23 20:06:06 -0700

    GPU ID assignment works properly on 5 firebox nodes w/ 2 GPUs each

--- mpi_hostfile_test/run_test.sh
+++ mpi_hostfile_test/run_test.sh
@@ -4,7 +4,7 @@ policy=--byslot #a19: {0, 1}; a20: {2, 3} ... this is default
 #hostfile=hostfiles/a19_a20_2slot.txt
 #hostfile=hostfiles/a18_a19_a20_allslot.txt
 #hostfile=hostfiles/a18_8slot.txt
-hostfile=hostfiles/f12_to_f16_1slot.txt
+hostfile=hostfiles/f12_to_f16_2slot.txt
 
-mpirun --hostfile $hostfile -np 5 $policy ./main
+/opt/openmpi/bin/mpirun --hostfile $hostfile -np 10 $policy ./main
 
