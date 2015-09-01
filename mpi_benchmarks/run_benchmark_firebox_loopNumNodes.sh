a4ca3e5908957db29e81cd59a4e5f35ad9329f1a
forresti <forresti@eecs.berkeley.edu>
2015-08-31 17:16:07 -0700

    ethernet benchmarks on firebox

--- mpi_benchmarks/run_benchmark_firebox_loopNumNodes.sh
+++ mpi_benchmarks/run_benchmark_firebox_loopNumNodes.sh
@@ -1,25 +1,38 @@
 
 probSize=7000000 #28MB / 4 -> in bytes. NiN param file size
-nRuns=1 #there's also an nRuns inside of the 'main' file... this is just for "meta-variance" analysis
-mkdir results
+nRuns=3 #there's also an nRuns inside of the 'main' file... this is just for "meta-variance" analysis
+outDir=results_allreduce
+#outDir=results_scatter_eth
+
+mkdir $outDir
 
 #TODO: figure out if/how I want to measure "1 gpu per slot; 2 gpu per slot."
 
-maxProcs=16
-for ((nProcs=16; nProcs<=$maxProcs; nProcs=$nProcs*2))
+#maxProcs=8
+#for ((nProcs=2; nProcs<=$maxProcs; nProcs=$nProcs*2))
+for nProcs in 2 4 8 12
 do
 
-    outF=results/out_${nProcs}_procs_allreduce.log
+    outF=$outDir/out_${nProcs}_procs.log
     rm $outF
 
-    #hostfile=../mpi_hostfile_test/hostfiles/a18_8slot.txt
-    #hostfile=../mpi_hostfile_test/hostfiles/f12_to_f16_1slot.txt
-    hostfile=../mpi_hostfile_test/hostfiles/firebox_1slot.txt
+    #hostfile=hostfiles/a18_8slot.txt
+    #hostfile=hostfiles/f12_to_f16_1slot.txt
+    hostfile=hostfiles/firebox_1slot.txt
+    #hostfile=hostfiles/firebox_1slot_eth.txt #1GB/s eth0 (MPI stuff hangs when using this)
 
     #firebox:
     for((i=0; i<$nRuns; i++))
     do
-        /opt/openmpi/bin/mpirun --hostfile $hostfile -np $nProcs ./main $probSize >> $outF
+        #defaults to Infiniband:
+        #/opt/openmpi/bin/mpirun --hostfile $hostfile -np $nProcs ./main $probSize >> $outF
+
+        #56 GB/s infiniband
+        /opt/openmpi/bin/mpirun --mca btl_tcp_if_include ib0 --mca btl tcp --hostfile $hostfile -np $nProcs ./main $probSize >> $outF 
+
+        #40GB/s ethernet (eth2):
+        #/opt/openmpi/bin/mpirun --mca btl_tcp_if_include eth2 --mca btl tcp --hostfile $hostfile -np $nProcs ./main $probSize >> $outF
+
     done
 done
 
